35ef5ccb3bc6a36c563a4bde82efbd73ad0207eb
Samuel B. Johnson <samuel.b.johnson@gmail.com>
2011-05-04 03:21:41 -0400

    Fix path problem for data update. Add php file to run update.

---
+++ utilities/updateNstedData.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Updating Database</title>
+</head>
+<body>
+	This script is not yet complete. Do not run without authorization.
+<?php include("gatherTblFiles.php.inc"); ?>
+</body>
+</html>
